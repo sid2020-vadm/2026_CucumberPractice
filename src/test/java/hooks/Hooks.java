4d8388977f91cf2be10c1fd6c643aa4deb4f45e1
sid2020-coder <skykite20@gmail.com>
2026-07-15 08:47:57 +0530

    Hook Edit user1

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -13,7 +13,6 @@ public class Hooks {
 
             // initialization
             System.out.println("initialization");
-            System.out.println("#####initialization######### ");
             initialized = true;
         }
     }
